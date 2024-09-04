06aae8c92faa7856b79d9b9e13c05d5ecb1d2c85
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-09-04 17:23:43 +0600

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,25 @@ dotenv.config({ path: envFiles[process.env.NODE_ENV] });
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    publicDir: 'public', // Include assets directory
     build: {
-        outDir: 'dist',
-        // Change the output directory if needed
-        minify: true, // Enable minification for production
-    },
+        rollupOptions: {
+            external: [
+                'assets/css/preloader.min.css',
+                'assets/css/bootstrap.min.css',
+                'assets/css/icons.min.css',
+                'assets/libs/jquery/jquery.min.js',
+                'assets/libs/bootstrap/js/bootstrap.bundle.min.js',
+                'assets/libs/metismenu/metisMenu.min.js',
+                'assets/libs/simplebar/simplebar.min.js',
+                'assets/libs/node-waves/waves.min.js',
+                'https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js',
+                'assets/libs/pace-js/pace.min.js',
+                'assets/libs/apexcharts/apexcharts.min.js',
+                'assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js',
+                'assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js',
+            ]
+        }
+    }
 })
+
+
