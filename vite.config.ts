43992a79720adc9bdd9b3113dd78ffb2ef562948
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-09-04 16:57:24 +0600

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,10 @@ dotenv.config({ path: envFiles[process.env.NODE_ENV] });
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    publicDir: 'public', // Include assets directory
+    build: {
+        outDir: 'dist',
+        // Change the output directory if needed
+        minify: true, // Enable minification for production
+    },
 })
